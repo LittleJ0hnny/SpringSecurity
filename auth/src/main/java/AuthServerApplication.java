1255d74039671469d190d7294abac0d9e180b6e5
OKravtsiv <kravtsivostap@gmail.com>
2018-10-29 18:21:15 +0200

    Added auth logic

---
+++ auth/src/main/java/AuthServerApplication.java
@@ -0,0 +1,7 @@
+import org.springframework.boot.SpringApplication;
+
+public class AuthServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServerApplication.class, args);
+    }
+}
